ddf704b1b1c08d7734f57f070d65266a70113e4f
Calla <callapatel@gmail.com>
2015-01-16 11:46:15 -0800

    posts editing functionality

--- app/js/controllers/posts.js
+++ app/js/controllers/posts.js
@@ -2,6 +2,7 @@ var postsControllerModule = angular.module('postsControllerModule', []);
 
 postsControllerModule.controller('postsController', ['$scope', '$http', function($scope, $http) {
   $scope.name = "posts contoller yay";
+  $scope.posts = [{id:2, title: 'sugar', time: 'monday'},{id:3, title: 'monkey', time:'tuesday'}]; 
 }]);
 
 
@@ -11,5 +12,5 @@ postsControllerModule.controller('newPostController', ['$scope', '$http', functi
 
 postsControllerModule.controller('postController', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
   $scope.postName = "this is the post view";
-  $scope.id = $stateParams.id; 
+  $scope.id = $stateParams.id;
 }]);
